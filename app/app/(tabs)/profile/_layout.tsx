d660df54d5947bcfcc3f145f795bf9098af80940
xarber <xarber616@gmail.com>
2026-02-17 16:33:29 +0100

    Fixed pages change offset issue

--- app/app/(tabs)/profile/_layout.tsx
+++ app/app/(tabs)/profile/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function ProfileLayout() {
   return (
     <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false, title: "Profile" }} />
+        <Stack.Screen name="index" options={{ title: "Profile", headerShown: true,headerTransparent: true,headerTitle: "" }} />
         <Stack.Screen name="settings" options={{ headerShown: true, title: "Settings" }} />
         <Stack.Screen name="login" options={{ presentation: "modal", headerShown: true, title: "Login" }} />
         <Stack.Screen name="logout" options={{ presentation: "modal", headerShown: true, title: "Logout" }} />
