39e1697f935069d90ba735d9afcdb66db7dec19d
xarber <xarber616@gmail.com>
2026-02-26 12:04:32 +0100

    update to test & server

--- server/db/middleware/test.js
+++ server/db/middleware/test.js
@@ -1,30 +1,32 @@
 const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
-module.exports = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+function connectionTest(req, res) {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
 
-  //set debug metrics
-  let authenticated = false;
-  let requestTime = new Date().getTime();
+    //set debug metrics
+    let authenticated = false;
+    let requestTime = new Date().getTime();
 
-  if (token) try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    authenticated = true;
-    
-    //if request URL is /connectiontest, end the request with debug data
-    if (req.path === '/connectiontest') {
-      return res.json({
+    if (token) try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        authenticated = true;
+    } catch (err) {
+        authenticated = 'invalid token';
+    }
+    return res.json({
         success: true,
         authenticated,
         serverTime: requestTime,
         //add more debug data if needed
-      });
-    }
+    });
+}
 
-    next();  // Pass to next (load data)
-  } catch (err) {
-    res.status(401).json({ error: 'Invalid token' });
-  }
-};
+router.get('/connectiontest', connectionTest);
+router.get('/', connectionTest);
+
+router.post('/connectiontest', connectionTest);
+router.post('/', connectionTest);
+
+module.exports = router;
