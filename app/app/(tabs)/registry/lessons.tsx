d53f3d3fbd780d8ca0e39d02bd8ecb9da8cb1b37
Xarber <xarber616@gmail.com>
2026-03-13 07:40:23 +0100

    display lessons

--- app/app/(tabs)/registry/lessons.tsx
+++ app/app/(tabs)/registry/lessons.tsx
@@ -1,12 +1,37 @@
 import { ScrollView, Text, View } from 'react-native';
 import { useTheme } from '@/constants/useThemes';
 import createStyling from '@/constants/styling';
-import { DataManager, useAppDataSync } from '@/data/datamanager';
+import { DataManager, LessonData, SubjectData, useAppDataSync, UserInfo } from '@/data/datamanager';
 import { useFocusEffect, useRouter } from 'expo-router';
 import { useCallback } from 'react';
 import i18n from '@/constants/i18n';
 import ActionButtons from '@/components/actionButtons';
 import { ActivityIndicator } from 'react-native';
+import DashboardItem from '@/components/dashboardItem';
+
+export function regroupLessonsByDate(lessonArray: {subjectid: string, data: LessonData[]}[]) {
+    let dateIndex = {};
+
+    for (let i = 0; i < lessonArray.length; i++) {
+        lessonArray[i].data.forEach((e) => {
+            (dateIndex as any)[e.date] ??= [];
+            (dateIndex as any)[e.date].push({
+                subjectid: lessonArray[i].subjectid,
+                data: e
+            });
+        })
+    }
+
+    for (let date in dateIndex) {
+        // sort individual dates for time
+        (dateIndex as any)[date].sort((a: any, b: any) => new Date(`${a.data.date}T${a.data.time}`).getTime() - new Date(`${b.data.date}T${b.data.time}`).getTime());
+    }
+
+    // sort dates
+    dateIndex = Object.entries(dateIndex).sort((a, b) => new Date(a[0]).getTime() - new Date(b[0]).getTime());
+
+    return dateIndex;
+}
 
 export default function LessonsTab() {
     const theme = useTheme();
@@ -20,12 +45,20 @@ export default function LessonsTab() {
         populate: ["subjects"]
     });
 
+    let defaultLessonsData = [{subjectid: "", data: [DataManager.lessonData.default]}];
+    const lessonData = useAppDataSync(userData.loading ? null : DataManager.lessonData.db, `${DataManager.lessonData.app}:${userData.data.settings.activeClassId}`, defaultLessonsData, {
+        classid: userData.data.settings.activeClassId
+    });
+
     useFocusEffect(
         useCallback(() => {
             userData.load();
             classData.load();
+            lessonData.load();
         }, [])
     );
+
+    const lessons = regroupLessonsByDate(lessonData.data) as any[];
 
     return (userData.loading || classData.loading) ? ( 
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -39,10 +72,19 @@ export default function LessonsTab() {
         <View style={[commonStyle.dashboardSection, { flex: 1 }]}>
             <ScrollView style={commonStyle.dashboardSection}>
                 <Text style={commonStyle.headerText}>{i18n.t("registry.lessons.header.text", {class: classData.data.name})}</Text>
-                <View style={commonStyle.card}>
-                    {classData.data.lessons.length === 0 ? (
+                <View>
+                    {lessons.length === 0 ? (
                         <Text style={commonStyle.text}>{i18n.t("registry.lessons.warn.nolessons.text")}</Text>
                     ) : null}
+                    {lessons.map((e, i) => (
+                        <DashboardItem title={e[0]} items={e[1].map((e: any) => {
+                            console.log(e)
+                            return {
+                                title: classData.data.subjects.find((s: SubjectData) => s._id == e.subjectid)?.name,
+                                description: `${e.data.description}\n\n${classData.data.teachers.find((t: UserInfo) => t._id == e.data.teacher)?.name}\n${new Date(`${e.data.date}T${e.data.time}`).toLocaleTimeString()}`,
+                            }
+                        })} />
+                    ))}
                 </View>
             </ScrollView>
             <ActionButtons items={[
