2868c55821dae1f5cc35f14611a060d50a4ff337
Xarber <xarber616@gmail.com>
2026-03-19 12:11:56 +0100

    Update icons & support app icon switch

--- app/constants/ThemeContext.tsx
+++ app/constants/ThemeContext.tsx
@@ -3,8 +3,16 @@ import { useColorScheme } from "react-native";
 import { Scheme } from "./colors";
 import { useAppDataSync, DataManager } from "@/data/datamanager";
 import { useUserData } from "@/data/UserDataContext";
+import { themeList } from "./colors";
+import { setAlternateAppIcon, getAppIconName, resetAppIcon } from "expo-alternate-app-icons";
 
 const ThemeContext = createContext<Scheme>("light");
+
+export function pascalCase(s: string) {
+    s = s.replace(/(\w)(\w*)/g,
+        function(g0,g1,g2){return g1.toUpperCase() + g2.toLowerCase();});
+    return s.replaceAll("-", "").replaceAll(" ", "");
+}
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
     const systemScheme = useColorScheme() ?? "light";
@@ -19,6 +27,18 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
             setScheme(systemScheme as Scheme);
         } else if (userTheme) {
             setScheme(userTheme);
+        }
+
+        if (themeList.special.includes(userTheme) || themeList.hidden.includes(userTheme)) {
+            let specialIconName = pascalCase(`icon-${userTheme}`);
+            if (getAppIconName() !== specialIconName) {
+                setAlternateAppIcon(specialIconName);
+            }
+        } else {
+            // let defaultIconName = pascalCase(`icon-default`);
+            if (getAppIconName() != null) {
+                resetAppIcon();
+            }
         }
     }, [userData.data.settings?.theme, systemScheme]);
 
