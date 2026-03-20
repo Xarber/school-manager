685e24cc141f451e8f81be03e3677acdff8e8fc1
Xarber <xarber616@gmail.com>
2026-03-20 20:09:24 +0100

    Add laboratory screens

--- app/app/(tabs)/ai/chat/[id].tsx
+++ app/app/(tabs)/ai/chat/[id].tsx
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native';
 import { useTheme } from '@/constants/useThemes';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 import createStyling from '@/constants/styling';
+import { Ionicons } from '@expo/vector-icons';
+import i18n from '@/constants/i18n';
 
 export default function ChatScreen() {
     const theme = useTheme();
@@ -12,8 +14,10 @@ export default function ChatScreen() {
     // You can use chatId to fetch chat data or perform other actions related to this specific chat
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={commonStyle.text}>This is the chat screen for chat ID: {chatId}</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10 }}>
+            <Ionicons name="flask-sharp" size={40} color={theme.text} />
+            <Text style={[commonStyle.headerText, {textAlign: "center"}]}>{i18n.t("beta.undeveloped.title")}</Text>
+            <Text style={[commonStyle.text, {textAlign: "center"}]}>{i18n.t("beta.undeveloped.message")}</Text>
         </View>
     );
 }
