09f3255cd08ee63e2e19d597027ea78eb256d664
Xarber <xarber616@gmail.com>
2026-03-24 18:40:31 +0100

    updates to timeline

--- app/components/timeline.tsx
+++ app/components/timeline.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@/constants/useThemes";
 import createStyling from "@/constants/styling";
 import { TouchableOpacity, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Feather, Ionicons } from "@expo/vector-icons";
 import { Pressable, Text } from "react-native";
 import { getTextColor } from "./dashboardItem";
 import i18n from "@/constants/i18n";
@@ -20,6 +20,7 @@ interface TimelineItem {
 interface TimelinePeriod {
     startTime: string;
     endTime: string;
+    break?: boolean;
     items: TimelineItem[];
 }
 
@@ -31,11 +32,37 @@ interface TimelineProps {
     }
     title?: string;
     periods: TimelinePeriod[];
+    renderSmallTime?: boolean;
+    renderBreaks?: boolean;
 }
 
 export default function Timeline(props: TimelineProps) {
     const theme = useTheme();
     const commonStyle = createStyling.createCommonStyles(theme);
+
+    if (props.renderBreaks != false) {
+        for (let i = 0;i < props.periods.length;i++) {
+            if (props.periods[i].items.length == 0) props.periods[i].break = true;
+            if (props.periods[i + 1] == null) break;
+            if (props.periods[i + 1].startTime != props.periods[i].endTime) {
+                const [h1, m1] = props.periods[i].endTime.split(":").map(Number);
+                const [h2, m2] = props.periods[i + 1].startTime.split(":").map(Number);
+                const [d1, d2] = [new Date(), new Date()];
+
+                d1.setHours(h1, m1, 0, 0);
+                d2.setHours(h2, m2, 0, 0);
+
+                if (d1.getTime() > d2.getTime()) continue;
+
+                props.periods.splice(i + 1, 0, {
+                    startTime: props.periods[i].endTime,
+                    endTime: props.periods[i + 1].startTime,
+                    break: true,
+                    items: []
+                });
+            }
+        }
+    }
 
     return (
         <View style={{ padding: 10 }}>
@@ -61,21 +88,41 @@ function TimelinePeriod({period, props, index}: {period: TimelinePeriod, props: 
 
     return (
         <View key={index} style={[{ display: "flex", flexDirection: "row", padding: 10, gap: 20 }, ((props.periods[index - 1]) && (props.periods[index - 1].endTime !== period.startTime)) ? { marginTop: 20 } : null]}>
-            <View style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 5 }}>
-                {((!props.periods[index - 1]) || (props.periods[index - 1].endTime !== period.startTime)) && <Text style={[commonStyle.text, { marginBottom: 15 }]}>{period.startTime}</Text>}
-                <View style={{ flex: 1, minHeight: 50, width: 2, borderRadius: 360, backgroundColor: theme.disabled }}></View>
-                <Text style={[commonStyle.text, { marginTop: 17, marginBottom: 2 }]}>{period.endTime}</Text>
+            <View style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 5, width: 50 }}>
+                {/* Start Time */}
+                {!period.break && <Text style={[commonStyle.text, { marginBottom: 15 }]}>{period.startTime}</Text>}
+
+                {/* Divider */}
+                <View style={[{ flex: 1, borderRadius: 360, display: "flex", justifyContent: "center", alignItems: "center" }, !period.break ? {minHeight: 50, width: 2, backgroundColor: theme.disabled} : {minHeight: 80, opacity: 0.4}]}>
+                    <Feather name={"x"} size={20} color={theme.disabled} />
+                </View>
+
+                {/* End Time */}
+                {!period.break && <Text style={[commonStyle.text, { marginTop: 17, marginBottom: 2 }]}>{period.endTime}</Text>}
             </View>
-            <View style={{ flex: 1, gap: 10 }}>
-                {period.items.map(( item, index ) => {
-                    return <TimelineItem key={index} item={item} index={index} />
-                })}
-                {props.edit?.editing && (
-                    <TouchableOpacity onPress={() => props.edit?.addItem({ startTime: period.startTime, endTime: period.endTime })} style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 5 }}>
-                        <Ionicons name="add" size={20} color={theme.text} />
-                        <Text style={commonStyle.text}>{i18n.t("components.timeline.add.text")}</Text>
-                    </TouchableOpacity>
-                )}
+            <View style={[{ flex: 1, display: "flex", flexDirection: "row", gap: 5 }]}>
+                <View style={{ flex: 1, gap: 10 }}>
+                    {period.break ? null : period.items.map(( item, index ) => {
+                        return <TimelineItem key={index} item={item} index={index} />
+                    })}
+                    {period.break && (
+                        <View style={{ display: "flex", flex: 1, alignItems: "center", justifyContent: "center", gap: 5 }}>
+                            <Ionicons name="albums-outline" size={20} color={theme.disabled} />
+                            <Text style={[commonStyle.text, { color: theme.disabled }]}>{i18n.t("components.timeline.noitems")}</Text>
+                        </View>
+                    )}
+                    {!period.break && props.edit?.editing && (
+                        <TouchableOpacity onPress={() => props.edit?.addItem({ startTime: period.startTime, endTime: period.endTime })} style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 5 }}>
+                            <Ionicons name="add" size={20} color={theme.text} />
+                            <Text style={commonStyle.text}>{i18n.t("components.timeline.add.text")}</Text>
+                        </TouchableOpacity>
+                    )}
+                </View>
+                <View style={{ display: !props.renderSmallTime ? "none" : "flex", flexDirection: "column", alignItems: "center", gap: 5 }}>
+                    <Text style={{color: theme.disabled, fontSize: 12}}>{period.startTime}</Text>
+                    <View style={{ flex: 1 }}></View>
+                    <Text style={{color: theme.disabled, fontSize: 12}}>{period.endTime}</Text>
+                </View>
             </View>
         </View>
     )
--- server/db/models/Class.js
+++ server/db/models/Class.js
@@ -5,7 +5,7 @@ const ClassSchema = new Schema({
     name: {type: String, required: true, maxlength: [30, "Name cannot exceed 30 characters"]},
     teachers: [{type: Schema.Types.ObjectId, ref: 'UserInfo'}],
     students: [{type: Schema.Types.ObjectId, ref: 'UserInfo'}],
-    schedule: [{type: WeekScheduleSchema, required: true}],
+    schedule: [{type: WeekScheduleSchema}],
     comunications: [{type: Schema.Types.ObjectId, ref: 'Comunication'}],
     material: [{type: Schema.Types.ObjectId, ref: 'Material'}],
     homework: [{type: Schema.Types.ObjectId, ref: 'Homework'}],
