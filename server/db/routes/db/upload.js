0cf261587c12fae8118b43f2302693e25d108dfc
Xarber <xarber616@gmail.com>
2026-03-25 09:32:26 +0100

    fix server crash (bad import)

--- server/db/routes/db/upload.js
+++ server/db/routes/db/upload.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const { UserInfo, UserData } = require('../../models/User');
-const paths = require('./paths.js');
+const paths = require('../paths.js');
 
 const router = express.Router();
 
