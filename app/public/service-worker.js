9af06de411afd1a957c6d3a6bd539d30131503de
Xarber <xarber616@gmail.com>
2026-03-29 17:20:33 +0200

    Update service worker

--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -1,2 +1,28 @@
-self.addEventListener('install', () => console.log('SW installed'));
-self.addEventListener('fetch', () => {});
+const CACHE = "school-manager-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest-en.json",
+        "/manifest-it.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(response => {
+        if (e.request.method === "GET") {
+          const clone = response.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return response;
+      });
+    })
+  );
+});
